e3cbfb411d0f430618c1b7a65efa2bd92ab8c061
Cara-Li Farrell <carali@me.com>
2024-02-29 10:13:14 -0500

    Add files via upload

---
+++ App 2 - I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Cara-Li Farrell on 2024-02-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
