1bbc2d56f0751ba8a376bb04267b4ad86314dc9b
Cara-Li Farrell <carali@me.com>
2024-02-28 23:20:23 -0500

    Add files via upload

---
+++ App 1 - I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Cara-Li Farrell on 2024-02-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
